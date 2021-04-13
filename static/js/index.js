6dcd9855412794ce47abd35c5a9e83baadfdfd37
Diegowla23 <75761393+Diegowla23@users.noreply.github.com>
2021-04-12 21:00:15 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -47,9 +47,9 @@ function LED1_Off(){
     // Once a connection has been made, make a subscription and send a message.
     console.log("Conectado...");
 	
-    client.subscribe("wlady_hp66@hotmail.com/test");
+    client.subscribe("wlady_hp66@hotmail.com/tema1");
     message = new Paho.MQTT.Message("hola desde la web");
-    message.destinationName = "lfrenteriax@hotmail.com/test1";
+    message.destinationName = "wlady_hp66@hotmail.com/tema1";
     client.send(message);
 	
   }
